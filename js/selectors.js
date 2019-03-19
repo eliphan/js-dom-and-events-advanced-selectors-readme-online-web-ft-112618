c94ebd68d458e09487451ad645160de0e6630c48
Elise Phan <aliceconnic@gmail.com>
2019-03-19 00:38:22 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img')[-1];
+}
+
+function ninjaBabySelector() {
+  return $)('img:baby-ninja')
+}
